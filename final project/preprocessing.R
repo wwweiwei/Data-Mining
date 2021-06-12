5758995ab745af7b1a86e2fda748a3c1437bbe08
wwweiwei <44032506+wwweiwei@users.noreply.github.com>
2021-06-12 21:36:46 +0800

    fix NA and format error

--- final project/preprocessing.R
+++ final project/preprocessing.R
@@ -1,23 +1,45 @@
+data <- read.csv("/Users/user/Desktop/googleplaystore.csv")
+
+## change format 
+## size M => 10^6, k => 10^3
+char_M <- "M"
+char_k <- "k"
+count_M = 0
+count_k = 0
+for(i in c(1:10840)){
+  if (grepl(char_M, data$Size[i], fixed = TRUE)){
+    new <- gsub("M", "", data$Size[i])
+    data$Size[i] <- as.double(new) * 1000000
+    count_M = count_M+1
+  } else if (grepl(char_k, data$Size[i], fixed = TRUE)){
+    new <- gsub("k", "", data$Size[i])
+    data$Size[i] <- as.double(new) * 1000 
+    count_k = count_k+1
+  }
+}
+class(data$Size)
+
 ## preprocessing
-data <- read.csv("/Users/user/Desktop/data.csv")
+#data <- read.csv("/Users/user/Desktop/data.csv")
 dim(data) ## 10840x16
 summary(data)
 head(data)
 
-## if size_clean == Varies with Device => size_clean = mean(size_clean)
+## if size == "Varies with Device" => size = mean(size)
 count = 0
 for(i in c(1:10840)){
-  if (data$Size_clean[i] == "Varies with device") 
-    data$Size_clean[i] = NA
+  if (data$Size[i] == "Varies with device"){
+    data$Size[i] <- NA
+  }
   else{
-    data$Size_clean[i] <- as.double(data$Size_clean[i])
+    data$Size[i] <- as.integer(data$Size[i])
     count = count + 1
   }
 }
-mean <- mean(as.double(data$Size_clean),na.rm = TRUE)
+mean <- mean(as.integer(data$Size),na.rm = TRUE)
 for(i in c(1:10840)){
-  if (is.na(data$Size_clean[i])) 
-    data$Size_clean[i] <- mean
+  if (is.na(data$Size[i])) 
+    data$Size[i] <- as.integer(mean)
 }
 
 ## rating == NA => delete the row
